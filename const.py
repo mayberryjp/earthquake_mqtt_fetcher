ba22b5ea8994812537069f122c1c6ea479353c32
mayberryjp <147527091+mayberryjp@users.noreply.github.com>
2024-01-16 13:42:19 +0900

    Update const.py

--- const.py
+++ const.py
@@ -9,13 +9,5 @@ SEND_MQTT=1
 LOAD_FILE=0
 RESET_EVERY_RUN=0
 PREFECTURE_JSON="prefecture.json"
-<<<<<<< HEAD
-DATABASE="earthquake.db"
-VERSION="v1.0.19"
-=======
 DATABASE="/data/earthquake.db"
-<<<<<<< HEAD
->>>>>>> fe14e04ae5b3e65f2f8ae5da3b8998cd54704254
-=======
 VERSION="v1.0.19"
->>>>>>> 9384c2f524df60fed86a78683327bc616887c46d
